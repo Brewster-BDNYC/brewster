96fa64eb19c3727501fc6bd70a5db206d2ae8543
canavarrete01 <100876091+canavarrete01@users.noreply.github.com>
2025-07-15 17:45:58 -0400

    Adding WISE1049A Files

---
+++ FostSlabFeDeck-WA.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+#SBATCH --nodes=3
+#SBATCH --ntasks=192
+#SBATCH --tasks-per-node=64
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=6G 
+#SBATCH --job-name=W1049A_FostSlabFeDeck-WA
+#SBATCH --output=W1049A_FostSlabFeDeck-WA.o%j
+#SBATCH --error=W1049A_FostSlabFeDeck-WA.e%j
+#SBATCH --partition=standard
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=cnavarrete@amnh.org
+#SBATCH --mail-type=ALL
+#SBATCH --export=NONE
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/home/3940/brewster_global/brewster/"
+vpkg_require my-sci-app/20201102
+
+#UD_QUIET_JOB_SETUP=YES                                                          	\
+#export UD_JOB_EXIT_FN_SIGNALS="SIGTERM EXIT"                                               	\
+#UD_PREFER_MEM_PER_CPU=YES                                                         	\
+#UD_REQUIRE_MEM_PER_CPU=YES                                                        	\
+#UD_MPI_RANK_DISTRIB_BY=CORE                                                        	\
+#UD_DISABLE_IB_INTERFACES=YES                                                       	\
+# Do standard Open MPI environment setup (networks, etc.)                                         	\
+
+. /opt/shared/slurm/templates/libexec/openmpi.sh
+cd /home/3940/brewster_global/brewster/
+
+#export TMPDIR="/lustre/xg-phy240309/users/3941/brewster_extra/tmpdir/$SLURM_JOBID.tmp"
+#mkdir -p $TMPDIR
+
+${UD_MPIRUN} python "/home/3940/brewster_global/brewster_v1.1/brewster/FostSlabFeDeck-WA.py" > ../log_files/W1049A_FostSlabFeDeck-WA.log
+
+#rm -r *core.*
+
+mpi_rc=$?
+
